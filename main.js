46397aa0dc70433e27ae9757f5461f3e3915495a
romairabri2 <romairabritom@gmail.com>
2020-09-08 17:15:16 -0400

    condicionales

--- main.js
+++ main.js
@@ -2,4 +2,14 @@ var nombre = "Romaira Brito";
 var altura = "156";
 var concatenacion = nombre + " " + altura;
 var datos = document.getElementById("datos");
-datos.innerHTML = `<h1>Soy la Caja de datos de ${nombre}</h1>`;
+datos.innerHTML = `<h1>Soy la Caja de datos</h1>
+                   <h2>Nombre: ${nombre}</h2>
+                   <h2>Mido: ${altura}</h2>
+`;
+
+if(altura >= 180){
+    datos.innerHTML += `<h1>Eres una persona ALTA</h1>`;
+}
+else {
+    datos.innerHTML += `<h1>Eres una persona BAJA</h1>`;
+}
