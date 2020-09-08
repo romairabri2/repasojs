0c132aa293b326cbc86c26cc04ff37603a5ebf33
romairabri2 <romairabritom@gmail.com>
2020-09-08 16:51:46 -0400

    first commit review JS

---
+++ main.js
@@ -0,0 +1,5 @@
+var nombre = "Romaira Brito";
+var altura = "156";
+var concatenacion = nombre + " " + altura;
+var datos = document.getElementById("datos");
+datos.innerHTML = `<h1>Soy la Caja de datos de ${nombre}</h1>`;
