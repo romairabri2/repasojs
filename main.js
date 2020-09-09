389e005f74702c76850926aa7658cc54ced70be3
romairabri2 <romairabritom@gmail.com>
2020-09-08 22:11:36 -0400

    repaso funciones

--- main.js
+++ main.js
@@ -14,7 +14,36 @@ else {
     datos.innerHTML += `<h1>Eres una persona BAJA</h1>`;
 }
 
-for(var i = 0; i<= 2020; i++){
+/*for(var i = 0; i<= 2020; i++){
     //bloque de instrucciones
     datos.innerHTML += `<h2>Estamos en el año: ${i}</h2>`;
+}*/
+
+function MuestraMiNombre(nombre, altura){
+    var misdatos =  `
+    <h1>Soy tu caja de datos =)</h1>
+    <h2>Mi nombre es: ${nombre}</h2>
+    <h3>Mido: ${altura} cm</h3>
+    `;
+    return misdatos;
 }
+
+function imprimir(){
+    var datos = document.getElementById("datos");
+    datos.innerHTML = MuestraMiNombre("Victor Robles Web 111", 190);
+}
+
+imprimir();
+
+
+var nombres = ["Victor", "Antonio", "Joaquin"];
+
+document.write(`<h1>Listado de nombres</h1>`);
+
+/*for(var i=0; i < nombres.length; i++) {
+    document.write(nombres[i] + '<br>');
+}*/
+
+nombres.forEach((nombre) => {
+    document.write(nombre + '<br>');
+});
